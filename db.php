0b736951eac294b462ddae0ffb823bd687d41192
Mohammad Alhelany <mohammad.alhelany@gmail.com>
2026-05-08 19:31:00 +0300

    db.php wasmer edit-2

--- db.php
+++ db.php
@@ -1,17 +1,22 @@
 <?php
 $host = 'db.fr-pari1.bengt.wasmernet.com';
-$db   = 'city_events';
-$user = 'root';
-$pass = '';
+$port = '10272';
+$db   = 'dbhzpDpPoH4WtxKNdTEBtmfx';
+$user = 'e040267b75268000f8198ca1b125';
+$pass = '069fe040-267b-7610-8000-eaeafc30152a';
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
 try {
-     $pdo = new PDO($dsn, $user, $pass, [
-         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-     ]);
+     $pdo = new PDO($dsn, $user, $pass, $options);
+     // echo "Connected successfully";
 } catch (\PDOException $e) {
-     die("Connection failed: " . $e->getMessage());
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 ?>
